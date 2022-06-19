3eae5795a4e26b6df6e1a20deea46491d6dab94f
Rohit Ramkumar <rohitramkumar13@gmail.com>
2022-06-19 14:52:19 -0500

    Update repo.dart to support whatsapp verification

--- lib/src/data/repo.dart
+++ lib/src/data/repo.dart
@@ -3,7 +3,9 @@ import 'package:twilio_phone_verify/src/model/twilio_reponse.dart';
 
 abstract class TwilioVerifyRepository {
   Future<TwilioResponse> sendSmsCode(String phone);
+  Future<TwilioResponse> sendWhatsappCode(String phone);
   Future<TwilioResponse> verifySmsCode(String phone, String code);
+  Future<TwilioResponse> verifyWhatsappCode(String phone, String code);
   Future<TwilioResponse> sendEmailCode(String email,{EmailChannelConfiguration? channelConfiguration});
   Future<TwilioResponse> verifyEmailCode(String email, String code);
 }
